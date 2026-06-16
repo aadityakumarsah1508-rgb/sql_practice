04abf49ba616489ef2b697b505ef6f3e4c4fffc0
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-16 14:49:08 +0530

    Solution of 1st question from Hackerrank

---
+++ day_01.sql
@@ -0,0 +1,9 @@
+-- Going to practice SQL question from hackerrank
+
+-- Question 1: - Revising the Select Query 1
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
+
+-- ==========================================================================================================
+
+-- Question 2: - 
