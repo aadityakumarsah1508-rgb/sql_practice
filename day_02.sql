9811b4d55e321cb52317261cb77b787d8d8b9083
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-17 13:52:07 +0530

    Solution of 6rd question from Hackerrank

---
+++ day_02.sql
@@ -0,0 +1,13 @@
+-- Question 4:- Select By ID
+
+SELECT * FROM CITY WHERE ID = '1661';
+
+-- ==============================================================
+
+-- Question 2: - Japanese City Attributes
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN';
+
+-- ==============================================================
+
+-- Question 3: - 
