d95effe8039700e24ebae5a30eaab5ade73a2cd8
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-18 18:07:30 +0530

    Solution of 10th question from Hackerrank

--- day_03.sql
+++ day_03.sql
@@ -8,4 +8,12 @@ SELECT DISTINCT CITY FROM STATION WHERE MOD(ID,2)= 0;
 SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
 -- =============================================================================================================
 
--- Question 10: - 
+-- Question 10: - Weather Observation 5
+
+SELECT city, length(city) from station
+order by length(city) asc, city  asc
+limit 1;
+SELECT city, length(city) from station
+ORDER by length(city) desc, city asc
+limit 1;
+-- ===============================================================================================================
