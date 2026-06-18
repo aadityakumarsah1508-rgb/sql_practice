b94648e52bd9a9568161f6c5771c3a6c472b976e
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-18 18:01:59 +0530

    Solution of 9th question from Hackerrank

--- day_03.sql
+++ day_03.sql
@@ -1,3 +1,11 @@
 -- Question 8:- Weather observation 3
 
 SELECT DISTINCT CITY FROM STATION WHERE MOD(ID,2)= 0;
+-- ============================================================================================================
+
+-- Question 9: - Weather Observation 4
+
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
+-- =============================================================================================================
+
+-- Question 10: - 
