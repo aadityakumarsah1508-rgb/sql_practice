95c4c9e384ec2d602eb704c0a02a9a9ec293b3c4
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-20 22:15:19 +0530

    Solution of 15th question from Hackerrank

--- day_05.sql
+++ day_05.sql
@@ -5,3 +5,12 @@ FROM STATION
 WHERE LEFT(CITY, 1) NOT IN ('A','E','I','O','U');
 
 -- ======================================================================================================
+-- Question 15: - Weather Observation 10 
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY NOT LIKE '%a'
+AND CITY NOT LIKE '%e'
+AND CITY NOT LIKE '%i'
+AND CITY NOT LIKE '%o'
+AND CITY NOT LIKE '%u';
