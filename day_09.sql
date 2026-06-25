43f8fbdbe04c4bf5decc051d3d536c64e45b4405
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-06-25 23:31:22 +0530

    Solution of 24th question from Hackerrank

--- day_09.sql
+++ day_09.sql
@@ -3,3 +3,11 @@
 SELECT COUNT(NAME)
 FROM CITY 
 WHERE POPULATION > 100000;
+
+-- ==========================================================================================
+
+-- Question 24:- Aggregation > Revising Aggregations - The Sum Function
+
+SELECT SUM(POPULATION)
+FROM CITY 
+WHERE DISTRICT = "California"; 
