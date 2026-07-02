a2666ee7111080bacf1520caf116ee8448748768
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-03 00:00:29 +0530

    Solution of 32nd question from Hackerrank

--- day_14.sql
+++ day_14.sql
@@ -1,3 +1,9 @@
 -- Question 31:- Aggregation > The Blunder
 
 SELECT CEIL(AVG(Salary * 1.0) - AVG(REPLACE(Salary, '0', '') * 1.0)) FROM EMPLOYEES;
+
+-- =========================================================================================
+
+-- Question 32:- Aggregation > Top Earners
+
+SELECT (months * salary), COUNT(*) FROM Employee GROUP BY (months * salary) ORDER BY (months * salary) DESC FETCH FIRST 1 ROWS ONLY;
