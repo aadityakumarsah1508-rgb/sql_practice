65189359814d1db2b21b581fb635ffe1e7e70829
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-06 22:50:13 +0530

    Approaching the question

--- day_15.sql
+++ day_15.sql
@@ -9,3 +9,19 @@ SELECT N,
     END
 FROM BST
 ORDER BY N;
+
+-- ======================================================================================================
+
+-- Question 33:- Advance Select > New Companies
+
+-- Select c.company_code,
+--         Lead_Manager.Count(lead_manager_code),
+--         Senior_Manager.count(senior_manager_code),
+--         Manager.Count(manager_code),
+--         Employee.count(employee_code)
+-- FROM Employee as e 
+--          Join Manager as m on e.manager_code = m.manager_code
+--          Join Senior_Manager as sm on e.senior_manager_code = sm.senior_manager_code
+--          Join Lead_Manager as lm on e.lead_manager_code = lm.lead_manager_code
+--          join Company as c on e.company_code = c.company_code
+-- order by company_code;
