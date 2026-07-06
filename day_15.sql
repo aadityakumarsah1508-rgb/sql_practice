5d2fec74ba1ffe5c63976fbcd8b157592d7cf5da
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-06 21:48:40 +0530

    Solution of 32nd question from Hackerrank

--- day_15.sql
+++ day_15.sql
@@ -3,7 +3,8 @@
 SELECT N,
     CASE 
         WHEN P IS NULL THEN "Root"
-        WHEN N NOT IN (SELECT P FROM BST WHERE P IS NOT NULL) THEN "Leaf"
+        -- WHEN N NOT IN (SELECT P FROM BST WHERE P IS NOT NULL) THEN "Leaf" -- if null condition is not written then it doesn't work
+        WHEN NOT EXISTS (SELECT 1 FROM BST b WHERE b.p = BST.n) THEN "Leaf"
         ELSE "Inner"
     END
 FROM BST
