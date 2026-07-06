042a782e4a30e956355c749f824f9b8eb96eb534
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-06 23:16:10 +0530

    Solution of 33rd question from Hackerrank

--- day_15.sql
+++ day_15.sql
@@ -25,3 +25,26 @@ ORDER BY N;
 --          Join Lead_Manager as lm on e.lead_manager_code = lm.lead_manager_code
 --          join Company as c on e.company_code = c.company_code
 -- order by company_code;
+
+SELECT 
+    c.company_code, 
+    c.founder, 
+    COUNT(DISTINCT lm.lead_manager_code), 
+    COUNT(DISTINCT sm.senior_manager_code), 
+    COUNT(DISTINCT m.manager_code), 
+    COUNT(DISTINCT e.employee_code)
+FROM 
+    Company c
+LEFT JOIN 
+    Lead_Manager lm ON c.company_code = lm.company_code
+LEFT JOIN 
+    Senior_Manager sm ON c.company_code = sm.company_code
+LEFT JOIN 
+    Manager m ON c.company_code = m.company_code
+LEFT JOIN 
+    Employee e ON c.company_code = e.company_code
+GROUP BY 
+    c.company_code, 
+    c.founder
+ORDER BY 
+    c.company_code ASC;
