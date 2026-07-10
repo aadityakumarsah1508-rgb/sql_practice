4c66809f27b7f135ec7067a368c58f976d16a4fa
Aaditya Kumar Sah <aadityakumarsah1508@gmail.com>
2026-07-10 20:28:13 +0530

    Soution of 35th questiton from Hackerrank

--- day_17.sql
+++ day_17.sql
@@ -1,5 +1,13 @@
 -- Question 35: - Aggegration > Top Earners
 
-Select max(months*salary) as earning, count(*)
-from Employee
-where months*salary = (select max(months*salary) from employee);
+-- Select max(months*salary) as earning, count(*)
+-- from Employee
+-- where months*salary = (select max(months*salary) from employee);
+
+SELECT MAX(months * salary) AS max_earnings,
+       COUNT(*) AS num_employees
+FROM Employee
+WHERE (months * salary) = (
+    SELECT MAX(months * salary)
+    FROM Employee
+);
